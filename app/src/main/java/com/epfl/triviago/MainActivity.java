f454bba8760904bbaf4f6c2f216adb50eed83b88
spaenleh <basile.spaenlehauer@epfl.ch>
2020-11-28 11:04:47 +0100

    ADD: Basile layout

--- app/src/main/java/com/epfl/triviago/MainActivity.java
+++ app/src/main/java/com/epfl/triviago/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // amazing
+
+
+        Button starttrivia = findViewById(R.id.StartTrivia);
+        starttrivia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TriviaQuestion.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/com/epfl/triviago/TriviaQuestion.java
+++ app/src/main/java/com/epfl/triviago/TriviaQuestion.java
@@ -3,12 +3,19 @@ package com.epfl.triviago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class TriviaQuestion extends AppCompatActivity {
+
+    private final String question = "{\"response_code\":0,\"results\":[{\"category\":\"General Knowledge\",\"type\":\"multiple\",\"difficulty\":\"easy\",\"question\":\"What is the Zodiac symbol for Gemini?\",\"correct_answer\":\"Twins\",\"incorrect_answers\":[\"Fish\",\"Scales\",\"Maiden\"]}]}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia_question);
+    }
+
+    public void answerRadioButtonClicked(View view) {
+
     }
 }
