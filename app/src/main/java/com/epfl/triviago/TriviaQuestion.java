6a8250c13f7d8f85fcb3fbbe79421e981c2646e7
spaenleh <basile.spaenlehauer@epfl.ch>
2020-11-28 10:27:33 +0100

    ADD: Basile Callback and Layout main

---
+++ app/src/main/java/com/epfl/triviago/TriviaQuestion.java
@@ -0,0 +1,14 @@
+package com.epfl.triviago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TriviaQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia_question);
+    }
+}
