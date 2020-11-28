46334e26a8a9313b5d5b4832c1a3e54af84594ed
Théophane Mayaud <theophanemayaud@gmail.com>
2020-11-28 12:37:13 +0100

    spinner works

--- app/src/main/java/com/epfl/triviago/ChooseNextWaypoint.java
+++ app/src/main/java/com/epfl/triviago/ChooseNextWaypoint.java
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -21,30 +24,48 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChooseNextWaypoint extends AppCompatActivity {
 
     List<String> spinnerValuesList =  new ArrayList<String>();
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_next_waypoint);
 
-
+        // Initialising the spinner values, should be taken from activity input values
+        spinner = (Spinner) findViewById(R.id.chooseNextWaypointSpinner);
         spinnerValuesList.add("item1");
         spinnerValuesList.add("item2");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, spinnerValuesList);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        Spinner spinner = (Spinner) findViewById(R.id.chooseNextWaypointSpinner);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                // Get the currently selected State object from the spinner
+                String selected_item = adapterView.getItemAtPosition(pos).toString();
+
+                // Now do something with it.
+                TextView selectedItemView = findViewById(R.id.selectedItemText);
+                selectedItemView.setText(selected_item);
+                selectedItemView.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
     }
-
-
 }
