83c59b8265bfa543f017f0a22a57f892d9daf239
Théophane Mayaud <theophanemayaud@gmail.com>
2020-11-28 12:05:14 +0100

    added spinner

--- app/src/main/java/com/epfl/triviago/ChooseNextWaypoint.java
+++ app/src/main/java/com/epfl/triviago/ChooseNextWaypoint.java
@@ -1,14 +1,50 @@
 package com.epfl.triviago;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChooseNextWaypoint extends AppCompatActivity {
+
+    List<String> spinnerValuesList =  new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_next_waypoint);
+
+
+        spinnerValuesList.add("item1");
+        spinnerValuesList.add("item2");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerValuesList);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.chooseNextWaypointSpinner);
+        spinner.setAdapter(adapter);
     }
+
+
 }
