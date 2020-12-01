4fc4e334f213604fb99705d67ccae41157d7e599
spaenleh <basile.spaenlehauer@epfl.ch>
2020-12-01 16:46:00 +0100

    test pull

--- app/src/main/java/com/epfl/triviago/TriviaQuestionActivity.java
+++ app/src/main/java/com/epfl/triviago/TriviaQuestionActivity.java
@@ -2,6 +2,7 @@ package com.epfl.triviago;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
@@ -12,13 +13,21 @@ public class TriviaQuestionActivity extends AppCompatActivity {
     private final String question = "{\"response_code\":0,\"results\":[{\"category\":\"General Knowledge\",\"type\":\"multiple\",\"difficulty\":\"easy\",\"question\":\"What is the Zodiac symbol for Gemini?\",\"correct_answer\":\"Twins\",\"incorrect_answers\":[\"Fish\",\"Scales\",\"Maiden\"]}]}";
 
     private Integer mResponse_index = -1;
+    private TriviaQuestion mTrivia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia_question);
 
+        // get intent for settings
 
+        // launch service
+//        Intent intent = new Intent(this, OTDBService.class);
+//        intent.setAction(OTDBService.ACTION_GET_QUESTION);
+//        intent.putExtra(OTDBService.DIFFICULTY_PARAM, "easy");
+//        intent.putExtra(OTDBService.CATEGORY_PARAM, 9); // General knowledge
+//        startService(intent);
     }
 
     public void answerRadioButtonClicked(View view) {
@@ -54,7 +63,13 @@ public class TriviaQuestionActivity extends AppCompatActivity {
         if (mResponse_index == -1) {
             Toast.makeText(this, "Please select a proposition", Toast.LENGTH_SHORT).show();
         } else {
-            // put here the logic to validate the question
+            View layout = findViewById(R.id.triviaParentLayout);
+            if (mResponse_index == 1) {
+                layout.setBackgroundColor(getResources().getColor(R.color.correct_answer));
+            }
+            else {
+                layout.setBackgroundColor(getResources().getColor(R.color.wrong_answer));
+            }
         }
     }
 }
