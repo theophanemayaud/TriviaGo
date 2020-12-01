f66c50a29753227301994c31dbb44c3c4e9e8c73
Théophane Mayaud <theophanemayaud@gmail.com>
2020-12-01 15:14:01 +0100

    maps now shows up !

--- app/src/main/java/com/epfl/triviago/ChooseNextWaypoint.java
+++ app/src/main/java/com/epfl/triviago/ChooseNextWaypoint.java
@@ -29,10 +29,12 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChooseNextWaypoint extends AppCompatActivity {
+public class ChooseNextWaypoint extends AppCompatActivity implements OnMapReadyCallback {
 
     List<String> spinnerValuesList =  new ArrayList<String>();
     Spinner spinner;
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +46,31 @@ public class ChooseNextWaypoint extends AppCompatActivity {
         spinnerValuesList.add("item1");
         spinnerValuesList.add("item2");
 
+        // Link the spinner and it's adapter with listener for modifications
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, spinnerValuesList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner.setOnItemSelectedListener(getListener());
+
+        // Obtain the SupportMapFragment and get notified
+        // when the map is ready to be used
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager()
+                .findFragmentById(R.id.GoogleMap);
+        mapFragment.getMapAsync(this); // will callback this.onMapReady
+    }
+
+
+
+    private AdapterView.OnItemSelectedListener getListener() {
+        return new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
                 // Get the currently selected State object from the spinner
                 String selected_item = adapterView.getItemAtPosition(pos).toString();
 
-                // Now do something with it.
+                // Now do something with it
                 TextView selectedItemView = findViewById(R.id.selectedItemText);
                 selectedItemView.setText(selected_item);
                 selectedItemView.setTextColor(Color.RED);
@@ -64,8 +80,11 @@ public class ChooseNextWaypoint extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parentView) {
                 // your code here
             }
+        };
+    }
 
-        });
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
