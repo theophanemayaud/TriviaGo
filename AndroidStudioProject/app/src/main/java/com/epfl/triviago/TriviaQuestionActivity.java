19f6252f46fc9f83742809ffc23c84000924dbf3
Théophane Mayaud <theophanemayaud@gmail.com>
2021-01-17 12:43:26 +0100

    Revert "re re revert commit to remove debug stuff : get those debug things back, before merge to main as made mistake to do changes in release branch and not main"
    This reverts commit 451106d90aaa42ec5a15af44dd91f376bbcba9b8.

--- AndroidStudioProject/app/src/main/java/com/epfl/triviago/TriviaQuestionActivity.java
+++ AndroidStudioProject/app/src/main/java/com/epfl/triviago/TriviaQuestionActivity.java
@@ -286,8 +286,6 @@ public class TriviaQuestionActivity extends AppCompatActivity {
         okButton.setVisibility(View.VISIBLE);
         checkAnswer.setVisibility(View.VISIBLE);
         checkAnswer.setText(R.string.select_answer);
-        // TODO: remove this for production
-        Toast.makeText(this, "ANSWER : " + mTrivia.mResponses.get(mTrivia.mCorrectIndex), Toast.LENGTH_LONG).show();
     }
 
     public void answerRadioButtonClicked(View view) {
